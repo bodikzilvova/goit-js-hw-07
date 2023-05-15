a5509852f1d979b0528e06135b2426f016042b23
bodikzilvova <103454649+bodikzilvova@users.noreply.github.com>
2023-05-15 12:44:34 +0300

    fix bugs

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,11 +3,6 @@ import { galleryItems } from './gallery-items.js';
 
 // Change code below this line
 
-
-
-console.log(galleryItems);
-
-
 const listGallery = document.querySelector(".gallery");
 
 const galleryItemsMarkup = galleryItems.map (({ preview, original, description}) =>`<li class="gallery__item">
@@ -18,18 +13,6 @@ const galleryItemsMarkup = galleryItems.map (({ preview, original, description})
 
 listGallery.insertAdjacentHTML("afterbegin", galleryItemsMarkup);
 
-
-listGallery.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    const targetLink = event.target;
-if (targetLink.nodeName !== "IMG"){
-return;
-}
-const originalUrl = targetLink.parentNode.href;
-targetLink.src = originalUrl;
-
-});
 
 const lightbox = new SimpleLightbox('.gallery a', {
     captionDelay: 250,
